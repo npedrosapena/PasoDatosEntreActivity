2951722086007db3858c64bdeef5a8ce0bf9de4a
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2014-11-20 12:43:08 +0100

    nuevas extensiones para jugar con los arraylist etc..

---
+++ app/src/main/java/com/colonias/nelson/pasodatosentreactivity/Agenda.java
@@ -0,0 +1,70 @@
+package com.colonias.nelson.pasodatosentreactivity;
+
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by nelson on 7/11/14.
+ */
+public class Agenda implements Serializable {
+
+    private ArrayList<Contacto> lista= new ArrayList<Contacto>();
+
+    public Agenda(){}
+
+    public Boolean guardarContacto(Contacto contacto)
+    {
+        if(lista.add(contacto)==true)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+
+    }
+
+    public Contacto buscarContacto(String contacto)
+    {
+        int indiceElemento=0;
+
+        for (Contacto con :lista)
+        {
+            if(con.getNombre().equals(contacto)==true)
+            {
+                return lista.get(indiceElemento);
+            }
+                indiceElemento += 1;
+        }
+        return null;
+
+    }
+
+    public void actualizarContacto(Integer id,Contacto c)
+    {
+        int indiceElemento=0;
+
+        for (Contacto con :lista)
+        {
+            if(con.getId().equals(id)==true)
+            {
+             lista.set(indiceElemento,c);
+            }
+            indiceElemento += 1;
+        }
+
+    }
+
+    public ArrayList arrayContactos()
+    {
+        return this.lista;
+    }
+    public String longitud()
+    {
+        return String.valueOf(lista.size());
+    }
+
+}
