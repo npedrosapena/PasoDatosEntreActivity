dc0ed6cabf823ca08126419be95752044e06ae40
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2014-11-20 12:42:12 +0100

    cambiado la llamada al activity3

--- app/src/main/java/com/colonias/nelson/pasodatosentreactivity/Activity1.java
+++ app/src/main/java/com/colonias/nelson/pasodatosentreactivity/Activity1.java
@@ -2,19 +2,98 @@ package com.colonias.nelson.pasodatosentreactivity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class Activity1 extends Activity {
 
+public class Activity1 extends Activity implements Serializable
+{
+   public Agenda a= new Agenda();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity1);
+
+        //controlamos si pulsa el boton añadir para crear un objeto nuevo
+
+        Button btnAdd= (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                EditText nombre=(EditText)findViewById(R.id.txtNombre);
+                EditText tlf= (EditText)findViewById(R.id.txtTelefono);
+
+                try
+                {
+
+                    Contacto contacto = new Contacto(nombre.getText().toString(), Integer.parseInt(tlf.getText().toString()));
+
+
+                    if(a.guardarContacto(contacto))
+                    {
+                        mensajeError("Contacto guardado correctamente");
+
+                        nombre.setText(null);
+                        tlf.setText(null);
+                    }else
+                    {
+                        mensajeError("Se produjo un fallo al guardar");
+                    }
+
+                }catch (Exception ex)
+                {
+                    mensajeError(ex.toString());
+                }
+
+            }
+        });
+
+        //botón para buscar un elemento del arrayList y mostrarlo en el segundo activity
+
+        Button btnList= (Button) findViewById(R.id.btnListar);
+        //final EditText txtBuscar= (EditText)findViewById(R.id.txtNomBusc);
+
+        btnList.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //
+                Bundle bundle= new Bundle();
+                Intent intento= new Intent(Activity1.this,Activity3.class);//del activity que estamos hasta el que queremos ir
+
+                ArrayList<Contacto> arrayContactos=a.arrayContactos();
+
+
+                bundle.putSerializable("agenda",a);
+                bundle.putSerializable("contactos", arrayContactos);
+                intento.putExtras(bundle);
+
+                startActivityForResult(intento,1);
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+
+        this.a=(Agenda)data.getExtras().getSerializable("agenda");
+
 
     }
 
@@ -49,4 +128,6 @@ public class Activity1 extends Activity {
         t.show();
 
     }
+
+
 }
